a52355377d4d00e1fcda76ef10e572ae303ab932
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-05-24 20:41:32 +0530

    Final with responsiveness

--- Education/index.js
+++ Education/index.js
@@ -1,7 +1,7 @@
 document.getElementById("back").addEventListener("click", shift);
 
 function shift(){
-  document.getElementById("shift").classList.add("shiftbottom");
+  document.getElementById("shift").classList.add("fade");
   setTimeout(function(){
     window.location.assign("../index.html");
   },950)
--- Skills/index.js
+++ Skills/index.js
@@ -5,4 +5,4 @@ function shift(){
   setTimeout(function(){
     window.location.assign("../index.html");
   },950)
-}
+};
