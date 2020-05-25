3d8d367fc562962f30ab128eebab1625155eef47
Jayanttokas <55356411+Jayanttokas@users.noreply.github.com>
2020-05-25 15:05:46 +0530

    made landing page

--- menu/menu/Education/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("back").addEventListener("click", shift);
-
-function shift(){
-  document.getElementById("shift").classList.add("fade");
-  setTimeout(function(){
-    window.location.assign("../index.html");
-  },950)
-}
